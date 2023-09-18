ba4f1527fcc7afbb8b6606ce82abe35ac02e067f
George <georgejoj28@gmail.com>
2023-09-18 16:56:53 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = 4;
+Math.random() + random;
+
+let num3 = 19;
+let num4 = 5
+const mod = num3 % num4;
+
+const max = Math.max(4, 5, 12, 9, 17, 20, 10)
